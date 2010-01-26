83f8370e793cbdc391147a0b926c2014167ef562
Jon Hoffman <jonhoffman@gmail.com>
2010-01-25 20:06:00 -0500

    first foray

---
+++ Classes/FirstPhoneGapAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  FirstPhoneGapAppDelegate.h
+//  FirstPhoneGap
+//
+//  Created by Jonathan Hoffman on 1/25/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface FirstPhoneGapAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstPhoneGap
+//
+//  Created by Jonathan Hoffman on 1/25/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"FirstPhoneGapAppDelegate");
+    [pool release];
+    return retVal;
+}
